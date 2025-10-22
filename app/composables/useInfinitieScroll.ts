42bf2ea788c6ce17524ed64a30d304bb1e65f25a
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-10-22 14:09:48 +0330

    feat(composable): Debounced onScroll for decrease function call

--- app/composables/useInfinitieScroll.ts
+++ app/composables/useInfinitieScroll.ts
@@ -1,13 +1,17 @@
+import { debounce } from '~/utils/debounce'
+
 export const useInfiniteScroll = () => {
   function onScroll() {
     const nearBottom = window.innerHeight + window.scrollY >= document.documentElement.scrollHeight - 100
     if (nearBottom) {
-      console.log('nearBottom')
+      console.log('here')
     }
   }
 
+  const debounced = debounce(onScroll, 1000)
+
   onMounted(() => {
-    window.addEventListener('scroll', onScroll)
+    window.addEventListener('scroll', () => debounced())
   })
 
   onUnmounted(() => {
