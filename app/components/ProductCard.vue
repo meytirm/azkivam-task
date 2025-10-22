fa3ce0a3a7590b7906bd7008e3cffce2f5033518
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-10-22 12:45:45 +0330

    feat(components): add `ProductCard` component with UI for product display

---
+++ app/components/ProductCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <UCard
+    variant="soft"
+    :ui="{
+      root: 'min-h-[300px]',
+    }"
+  >
+    <NuxtImg :src="product.imageUrl" />
+    <div>{{ product.name }}</div>
+    <div>
+      <div>شروع قیمت از:</div>
+      <div>{{ product.minPrice }} <span>تومان</span></div>
+    </div>
+  </UCard>
+</template>
+
+<script setup lang="ts">
+import type { Product } from '~~/types/products'
+
+defineProps<{ product: Product }>()
+</script>
+
+<style scoped>
+
+</style>
