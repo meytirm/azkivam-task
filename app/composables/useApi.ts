037e6966e54feb96daacf8411d212719f2758b8f
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-10-22 10:31:58 +0330

    feat(composable/api): create an instance of ofetch and create a layer and export get and post for better use

---
+++ app/composables/useApi.ts
@@ -0,0 +1,36 @@
+import { ofetch, type FetchOptions } from 'ofetch'
+
+type RequestType = BodyInit | Record<string, never> | null | undefined
+
+export const useApi = () => {
+  const config = useRuntimeConfig()
+  const baseUrl = config.public.apiBase
+
+  const api = ofetch.create({
+    baseURL: baseUrl,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    timeout: 10000,
+  })
+
+  function get<ResponseType>(url: string, options?: FetchOptions<'json'>): Promise<ResponseType> {
+    return api<ResponseType>(url, {
+      method: 'GET',
+      ...options,
+    })
+  }
+
+  function post<ResponseType>(url: string, options?: FetchOptions<'json'>, body?: RequestType): Promise<ResponseType> {
+    return api<ResponseType>(url, {
+      method: 'GET',
+      body: body,
+      ...options,
+    })
+  }
+
+  return {
+    get,
+    post,
+  }
+}
