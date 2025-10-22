335c6164b0f75f9177ab32dbb740d29039af42c8
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-10-22 08:37:09 +0330

    chore(nuxt-config): Add configs for type check linter and api base url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-
   modules: [
     '@nuxt/eslint',
     '@nuxt/ui',
     '@nuxt/test-utils',
-    '@nuxt/image'
-  ]
+    '@nuxt/image',
+  ],
+  devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      apiBase: 'https://interview-api.azkivam.com/api/v1',
+    },
+  },
+  compatibilityDate: '2025-07-15',
+  typescript: {
+    typeCheck: true,
+  },
+  eslint: {
+    config: {
+      stylistic: {
+        indent: 2,
+        semi: false,
+        quotes: 'single',
+      },
+    },
+  },
 })
