1d2f7dd132cd2a43fa69a2e35f92680ca0f52ac7
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-10-22 12:45:57 +0330

    feat(components): add `ProductsRack` component for displaying a grid of products

---
+++ app/components/ProductsRack.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="relative grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+      <ProductCard
+        v-for="product in products"
+        :key="product.id"
+        :product="product"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { Product } from '~~/types/products'
+import ProductCard from '~/components/ProductCard.vue'
+
+defineProps<{ products: Product[] }>()
+</script>
+
+<style scoped>
+
+</style>
