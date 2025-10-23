e517181a38d5ff39b200e677f38c7fb0bbee290a
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-10-23 08:51:42 +0330

    feat(components): add `ProductSkeleton` component for loading placeholders in product grid

---
+++ app/components/ProductSkeleton.vue
@@ -0,0 +1,42 @@
+<template>
+  <div
+    class="relative grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8"
+  >
+    <div class="p-8">
+      <USkeleton class="h-[200px]" />
+      <div class="grid gap-2 mt-4">
+        <USkeleton class="h-4 w-full" />
+        <USkeleton class="h-4 w-full" />
+      </div>
+    </div>
+    <div class="p-8">
+      <USkeleton class="h-[200px]" />
+      <div class="grid gap-2 mt-4">
+        <USkeleton class="h-4 w-full" />
+        <USkeleton class="h-4 w-full" />
+      </div>
+    </div>
+    <div class="p-8">
+      <USkeleton class="h-[200px]" />
+      <div class="grid gap-2 mt-4">
+        <USkeleton class="h-4 w-full" />
+        <USkeleton class="h-4 w-full" />
+      </div>
+    </div>
+    <div class="p-8">
+      <USkeleton class="h-[200px]" />
+      <div class="grid gap-2 mt-4">
+        <USkeleton class="h-4 w-full" />
+        <USkeleton class="h-4 w-full" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
