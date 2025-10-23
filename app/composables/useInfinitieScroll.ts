0d8d109f67bc5bb02882b89c5a35452013790588
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-10-23 08:51:29 +0330

    feat(composable): enhance `useInfiniteScroll` with async data fetching and improved state management

--- app/composables/useInfinitieScroll.ts
+++ app/composables/useInfinitieScroll.ts
@@ -1,10 +1,30 @@
 import { debounce } from '~/utils/debounce'
+import type { UnwrapRef } from 'vue'
 
-export const useInfiniteScroll = () => {
-  function onScroll() {
+export function useInfiniteScroll<T>(
+  asyncFunc: (page: number, size: number) => Promise<T[]>,
+  initialData: T[] = [],
+  pageSize: number = 12,
+) {
+  const page = ref(1)
+  const items = ref<T[]>([...initialData])
+  const loading = ref(false)
+
+  async function onScroll() {
     const nearBottom = window.innerHeight + window.scrollY >= document.documentElement.scrollHeight - 100
     if (nearBottom) {
-      console.log('here')
+      loading.value = true
+      page.value++
+      try {
+        const res = await asyncFunc(page.value, pageSize)
+        items.value.push(...res as UnwrapRef<T[]>)
+      }
+      catch (e) {
+        console.log(e)
+      }
+      finally {
+        loading.value = false
+      }
     }
   }
 
@@ -17,4 +37,6 @@ export const useInfiniteScroll = () => {
   onUnmounted(() => {
     window.removeEventListener('scroll', onScroll)
   })
+
+  return { items, loading }
 }
