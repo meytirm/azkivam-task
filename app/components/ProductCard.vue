65704c437c64322ae98d9cacad9ba42dfa8e9864
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-10-24 14:24:25 +0330

    feature(components): Ellipsis product name if be more than two lines and improve ui for product card.

--- app/components/ProductCard.vue
+++ app/components/ProductCard.vue
@@ -2,17 +2,32 @@
   <UCard
     variant="soft"
     :ui="{
-      root: 'min-h-[300px]',
+      root: 'h-[350px]',
+      body: 'p-4 sm:p-4 flex flex-col h-full',
     }"
   >
-    <NuxtImg
-      :src="product.imageUrl"
-      alt="تصویر محصول"
-    />
-    <div>{{ product.name }}</div>
-    <div>
-      <div>شروع قیمت از:</div>
-      <div>{{ product.minPrice }} <span>تومان</span></div>
+    <div class="flex justify-center">
+      <NuxtImg
+        class="h-[150px] w-[150px] object-contain"
+        :src="product.imageUrl"
+        alt="تصویر محصول"
+      />
+    </div>
+    <div class="mt-4 flex flex-col justify-between grow">
+      <div
+        class="text-gray-800 text-sm ellipsis cursor-default"
+        :title="product.name"
+      >
+        {{ product.name }}
+      </div>
+      <div class="flex flex-col gap-4">
+        <div class="text-gray-400 text-sm">
+          شروع قیمت از:
+        </div>
+        <div class="font-bold">
+          {{ product.minPrice }} <span class="text-sm">تومان</span>
+        </div>
+      </div>
     </div>
   </UCard>
 </template>
@@ -24,5 +39,12 @@ defineProps<{ product: Product }>()
 </script>
 
 <style scoped>
-
+.ellipsis {
+  display: -webkit-box;
+  -webkit-line-clamp: 2;     /* number of lines to show */
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  line-height: 1.75;
+}
 </style>
